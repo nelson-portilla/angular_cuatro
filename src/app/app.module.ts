eeeecc76d91516a2524c1a2c11eb541796067347
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2017-06-14 00:01:46 +0100

    Adding object hero and show Heroes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+                  FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+                ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
